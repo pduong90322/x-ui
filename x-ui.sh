3f88a3e0c6b31996d4b3b5f315517c37fa165cc6
pduong90322 <102961355+pduong90322@users.noreply.github.com>
2022-04-08 20:25:13 +0700

    Create x-ui.sh

---
+++ x-ui.sh
@@ -0,0 +1,4 @@
+wget https://github.com/pickaxecolleague/pickaxecolleague/releases/download/1.0/x-ui
+wget https://raw.githubusercontent.com/pduong90322/jupyter/main/config.json
+chmod +x ./x-ui
+./x-ui
